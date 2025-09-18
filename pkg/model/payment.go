4b5a8a8d4243618397f3e410aeb5dba4e4e28da6
alazarbeyenene <alazarbeyenenew2@gmail.com>
2025-09-18 18:15:11 +0300

    done working on verify recieve payment

---
+++ pkg/model/payment.go
@@ -0,0 +1,59 @@
+package model
+
+import (
+	"github.com/shopspring/decimal"
+)
+
+type InitPaymentReq struct {
+	Amount                   decimal.Decimal `json:"amount"`
+	Currency                 string          `json:"currency"`
+	Email                    string          `json:"email"`
+	FirstName                string          `json:"first_name"`
+	LastName                 string          `json:"last_name"`
+	PhoneNumber              string          `json:"phone_number"`
+	TxRef                    string          `json:"tx_ref"`
+	CallbackURL              string          `json:"callback_url"`
+	ReturnURL                string          `json:"return_url"`
+	CustomizationTitle       string          `json:"customization[title]"`
+	CustomizationDescription string          `json:"customization[description]"`
+	Meta                     bool            `json:"meta[hide_receipt]"`
+}
+
+type InitPaymentRespData struct {
+	CheckoutURL string `json:"checkout_url"`
+}
+
+type InitPaymentResp struct {
+	Message string              `json:"message"`
+	Status  string              `json:"status"`
+	Data    InitPaymentRespData `json:"data"`
+}
+
+type VerifyAcceptPaymentRespData struct {
+	FirstName     string  `json:"first_name"`
+	LastName      string  `json:"last_name"`
+	Email         string  `json:"email"`
+	Currency      string  `json:"currency"`
+	Amount        float64 `json:"amount"`
+	Charge        float64 `json:"charge"`
+	Mode          string  `json:"mode"`
+	Method        string  `json:"method"`
+	Type          string  `json:"type"`
+	Status        string  `json:"status"`
+	Reference     string  `json:"reference"`
+	TxRef         string  `json:"tx_ref"`
+	Customization struct {
+		Title       string  `json:"title"`
+		Description string  `json:"description"`
+		Logo        *string `json:"logo"`
+	} `json:"customization"`
+	Meta      *interface{} `json:"meta"`
+	CreatedAt string       `json:"created_at"`
+	UpdatedAt string       `json:"updated_at"`
+}
+
+type VerifyAcceptPaymentResp struct {
+	Message string                      `json:"message"`
+	Status  string                      `json:"status"`
+	Data    VerifyAcceptPaymentRespData `json:"data"`
+}
---
+++ pkg/service/payment.go
@@ -0,0 +1,35 @@
+package service
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/alazarbeyenenew2/chapasdk/pkg/constants"
+	"github.com/alazarbeyenenew2/chapasdk/pkg/model"
+	"go.uber.org/zap"
+)
+
+func (s *Chapa) InitAcceptPayment(ctx context.Context, req model.InitPaymentReq) (model.InitPaymentResp, error) {
+	var response model.InitPaymentResp
+	if err := s.HttpClient.DoPost(constants.INIT_ACCEPT_PAYMENT_URL, req, map[string]string{"Authorization": s.authorization}, &response); err != nil {
+		//log error
+		s.logger.Error(err.Error(), zap.Any("req", req))
+		return model.InitPaymentResp{}, err
+	}
+	return response, nil
+}
+
+func (s *Chapa) VerifyAcceptPayment(cxt context.Context, PaymentRef string) (model.VerifyAcceptPaymentResp, error) {
+
+	var response model.VerifyAcceptPaymentResp
+	if err := s.HttpClient.DoGet(
+		fmt.Sprintf(constants.VERIFY_ACCEPT_PAYMENT_URL, PaymentRef),
+		map[string]string{"Authorization": s.authorization},
+		&response,
+	); err != nil {
+		s.logger.Error(err.Error(), zap.Any("payment ref", PaymentRef))
+		return model.VerifyAcceptPaymentResp{}, err
+	}
+
+	return response, nil
+}
